cb4395bd857fa591b27ddabcf3f5e8cc1ebba7e5
Ale2610 <ale.proto00@gmail.com>
2015-07-31 19:52:45 +0200

    Update sPhone.lua

--- src/sPhone.lua
+++ src/sPhone.lua
@@ -1,1 +1,35 @@
 sPhone = {}
+
+local function sPhone.crash(err)
+  os.pullEvent = os.pullEventRaw
+  if not err then
+    err = "Unknown"
+  end
+  term.setBackgroundColor(colors.black)
+  term.clear()
+  term.setCursorPos(1,2)
+  term.setTextColor(colors.white)
+  print( 
+    " ###",
+    " #  ",
+    " ###",
+    "   #",
+    " ###",
+  )
+  print("")
+  print("sPhone crash: ")
+  print(err)
+  while true do
+    sleep(3600)
+  end
+end
+
+function kernel()
+  
+end
+
+local ok, err = pcall(kernel)
+
+if not ok then
+  sPhone.crash(err)
+end
