ba7db152ff2efbfed6ee28f15a41ef27daf36852
Ale32bit <ale.proto00@gmail.com>
2015-09-21 21:35:43 +0200

    Update info.lua

--- src/apps/system/info.lua
+++ src/apps/system/info.lua
@@ -8,7 +8,7 @@ end
 local function header()
 	clear()
 	local w, h = term.getSize()
-	paintutils.drawLine(1,1,w,1, colors.gray)
+	paintutils.drawLine(1,1,w,1, colors.blue)
 	term.setTextColor(colors.white)
 	term.setCursorPos(1,1)
 	write(" "..sPhone.user)
