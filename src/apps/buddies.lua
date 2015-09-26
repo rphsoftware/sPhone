ab8d806cdaa2300c1ba9f0e1b7e8d27913e136fa
Ale32bit <ale.proto00@gmail.com>
2015-09-26 11:30:40 +0200

    Create buddies.lua

---
+++ src/apps/buddies.lua
@@ -0,0 +1,8 @@
+local function clear()
+  term.setBackgroundColor(colors.brown)
+  term.setTextColor(colors.white)
+  term.clear()
+  term.setCursorPos(1,1)
+end
+clear()
+sPhone.winOk("Work In", "Progress")
