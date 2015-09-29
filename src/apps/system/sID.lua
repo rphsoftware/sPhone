369bb4d3df54f6a2fc1d7366dfcc245471f17eaf
Ale32bit <ale.proto00@gmail.com>
2015-09-29 21:36:56 +0200

    Update sID.lua

--- src/apps/system/sID.lua
+++ src/apps/system/sID.lua
@@ -120,7 +120,7 @@ while true do
 		menu = {
 			"Logout",
 		}
-		 _, id = ui.menu(menu, "Sertex ID", true)
+		 _, id = sPhone.menu(menu, "Sertex ID", true)
 		if id == 1 then
 		 	fs.delete("/.sPhone/config/.sIDpw")
 		elseif id == 0 then
@@ -131,7 +131,7 @@ while true do
 			"Login",
 			"Register",
 		}
-		_, id = ui.menu(menu, "Sertex ID", true)
+		_, id = sPhone.menu(menu, "Sertex ID", true)
 		if id == 1 then
 			login()
 		elseif id == 2 then
