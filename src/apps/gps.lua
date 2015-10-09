09a3843b853f5277ee30b708599d3c3f868efbe4
Ale32bit <ale.proto00@gmail.com>
2015-10-09 17:27:13 +0200

    Update gps.lua

--- src/apps/gps.lua
+++ src/apps/gps.lua
@@ -2,4 +2,8 @@ local x, y, z = gps.locate(0)
 if not x then
   x, y, z = "?", "?", "?"
 end
-sPhone.winOk("X Y Z", math.ceil(x).." "..math.ceil(y).." "..math.ceil(z))
+if tonumber(x) then
+  sPhone.winOk("X Y Z", math.ceil(x).." "..math.ceil(y).." "..math.ceil(z))
+else
+  sPhone.winOk("X Y Z","? ? ?")
+end
