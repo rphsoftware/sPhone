156a5b8c70e5b572019af3f7c80404b5b9141e76
Ale32bit <ale.proto00@gmail.com>
2015-11-11 17:44:08 +0100

    Update sertexID.lua

--- src/apis/sertexID.lua
+++ src/apis/sertexID.lua
@@ -1,9 +1,8 @@
 --[[
-  Sertex ID API - Made by Ale32bit
-  PHP by Luca_S
+  Sertex ID API - Made by Sertex-Team
 ]]--
 
-local SERVER = "http://sertex.esy.es/"
+local SERVER = "http://sertex.x10.bz/"
 
 function status()
   local isUp = http.get(SERVER.."status.php").readAll()
