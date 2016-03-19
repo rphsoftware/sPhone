01063d2550b3427552b24babb0f795bef0be6ec0
Ale32bit <ale.proto00@gmail.com>
2016-03-19 23:31:17 +0100

    Update info.lua

--- src/apps/system/info.lua
+++ src/apps/system/info.lua
@@ -4,26 +4,11 @@ local function clear()
   term.clear()
   term.setCursorPos(1,1)
 end
-
-local function header()
-	clear()
-	local w, h = term.getSize()
-	paintutils.drawLine(1,1,w,1, colors.blue)
-	term.setTextColor(colors.white)
-	term.setCursorPos(1,1)
-	write(" "..sPhone.user)
-	term.setCursorPos(w,1)
-	write("X")
-	term.setCursorPos(1,2)
-	term.setBackgroundColor(colors.white)
-	term.setTextColor(colors.black)
-end
-header()
-
-term.setCursorPos(1,3)
-visum.align("center", "  Info",false,3)
+clear()
+sPhone.header("Info", "X")
 print("")
 print("ID: "..os.getComputerID())
+print("User: "..sPhone.user)
 if os.getComputerLabel() then
 	print("Label: "..os.getComputerLabel())
 end
